4f3feabf35bcb762cabc74725a48a75a0e2f8fb5
Carlos Franco <cdf.175@gmail.com>
2023-08-28 09:48:24 -0300

    Agregar archivos de proyecto.

---
+++ TpIntegradorSofttek/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TpIntegradorSofttek.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController
+    {
+        
+
+    }
+}
